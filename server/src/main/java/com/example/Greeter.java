d2be488a3bc5a558339e31a3d4f2d6867ac47108
cursomg154 <cursomg154@mtin.es>
2018-05-08 12:52:28 +0200

    greeter parametro final

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,8 @@ public class Greeter {
 
   }
 
-
-  final public String greet(final String someone) {
+/** Comentario para JavaDoc */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
